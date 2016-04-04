586fdf310984cbd2a680c16d9525f0503e9a6ea2
Janimations <janimations@gmx.de>
2016-04-04 16:16:50 -0700

    data-objects sucessfully populated from demo-rss-data

--- rssConverter/convertTest.js
+++ rssConverter/convertTest.js
@@ -49,43 +49,74 @@ rssData = [
                         ];
 
 
-        //****** pull title and fish data for each dam:
+//****** STEP 1 --- pull title and fish data for each dam pushes selected data into rawData-Array
 
-        var makeStripData = function () {
+var makeStripData = function () {
 
-                for (var i = 0; i < rssData.length; i++) {   // STEP 1 pushes selected data into rawData-Array
-                    var stripData = [];
+        for (var i = 0; i < rssData.length; i++) {
+            var stripData = [];
 
-                    stripData.push(rssData[i].title);
-                    stripData.push(rssData[i].content);
-                    rawData.push(stripData);
+            stripData.push(rssData[i].title);
+            stripData.push(rssData[i].content);
+            rawData.push(stripData);
+        }
+
+};
+
+//****** STEP 2 --- turn title-property from rawData into new project- and date-properties in chinookCohoData-Objects:
+var makeProjectDate = function() {
+
+    for (var i = 0; i < rawData.length; i++) {
+
+        var titleSplit = rawData[i][0].split(" ");  // this accesses the title info in rawData at index [0]
+
+        if (titleSplit[0] == "BONNEVILLE") {
+            chinookCohoData[i].project = "BON";
+        } else {
+            chinookCohoData[i].project = "TDA";
+        }
+
+        chinookCohoData[i].date = titleSplit.pop();  // pops off last item (which should be date string "mm/dd/yyy")
+    }  // for-loop close
+
+} // makeProjectDate() close
+
+
+
+
+//****** STEP 3 --- turn content property from rawData into new Chinook and Coho properties...
+var makeChinookCoho = function() {
+
+    for (var i = 0; i < rawData.length; i++) {
+
+
+        var contentSplit = rawData[i][1].split("; ");    // this accesses the content info in rawData at index [1],
+                                                        // contentSplit is now an array ["Chinook Adult 90", "Chinook Jack 2"....]
+
+        for (var k = 0; k < contentSplit.length; k++) {     // lops through contentSplit-Array
+            var x = contentSplit[k].split(" ");     // creates array with ["Chinook", "Adult", "90"]
+
+                if (x[0] == "Chinook" && x[1] == "Adult") {     // if x[0] is either ("Chinook" && "Adult") or ("Coho" && "Adult") pop off the number...
+                    chinookCohoData[i].Chinook = x.pop();
+                } else if (x[0] == "Coho" && x[1] == "Adult") {
+                    chinookCohoData[i].Coho = x.pop();
                 }
+        } // inner for-loop close
 
-        };
+    }  // for-loop close
 
-        //****** turn title-property into new project- and date-properties...
-        var makeProjectDate = function() {
-
-            for (var i = 0; i < rawData.length; i++) {
-
-                var titleSplit = rawData[i][0].split(" ");
-
-                if (titleSplit[0] == "BONNEVILLE") {
-                    chinookCohoData[i].project = "BON";
-                } else {
-                    chinookCohoData[i].project = "TDA";
-                }
-
-                chinookCohoData[i].date = titleSplit.pop();  // pops off last item (which should be date string "mm/dd/yyy")
-            }  // for-loop close
-
-        } // makeProjectDate() close
+} // makeChinookCoho() close
 
 
-
+/*==============================================================
+                           TEST AREA:
+===============================================================*/
 
         makeStripData();
-        console.log('Step 1 rawData:', rawData);
+        console.log('Step 1 --- rawData:', rawData);
 
         makeProjectDate();
-        console.log('Step 2 chinookCohoData: ', chinookCohoData);
+        console.log('Step 2 --- chinookCohoData: ', chinookCohoData);
+
+        makeChinookCoho();
+        console.log('Step 3 --- chinookCohoData: ', chinookCohoData);
